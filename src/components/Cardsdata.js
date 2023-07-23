5dfb034594c8813190db0ea8a81e620e38f633be
Ganesh Govind Taur <96427947+taurganesh31@users.noreply.github.com>
2023-07-23 14:52:17 +0530

    Add files via upload

---
+++ src/components/Cardsdata.js
@@ -0,0 +1,112 @@
+const Cardsdata = [
+    {
+        id: 1,
+        rname: "Google Pixel 7a",
+        imgdata: "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/y/u/3/-original-imaggsuddwubypxp.jpeg?q=70",
+        address: "8 GB RAM - 128 GB ROM || 64MP (OIS) + 13MP - 13MP Front Camera || 4300 mAh Battery || Tensor G2 Processor",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 1175 + order placed  recently",
+        price: 34999,
+        rating: "3.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+        qnty:0
+    },
+    {
+        id: 2,
+        rname: "Google Pixel 6a",
+        imgdata: "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/s/y/0/-original-imaggbrbxkqr3v3u.jpeg?q=70",
+        address: "8 GB RAM - 256 GB ROM || 64MP + 32MP + 8MP - 32MP Front Camera || 5000 mAh Battery ||\n Dimensity 7050 Processor ",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 2525 + order placed  recently",
+        price: 24999,
+        rating: "3.9",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 3,
+        rname: "Nothing Phone 1",
+        imgdata: "https://rukminim2.flixcart.com/image/312/312/l5h2xe80/mobile/5/x/r/-original-imagg4xza5rehdqv.jpeg?q=70",
+        address: "8 GB RAM | 256 GB ROM  || 4500 mAh Lithium-ion Battery || Qualcomm Snapdragon 778G+ Processor ",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 650 + order placed recently",
+        price: 27999,
+        rating: "4.2",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 4,
+        rname: "Realme 10 pro",
+        imgdata: "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/8/u/s/-original-imagkp8yreabrzh2.jpeg?q=70",
+        address: "8 GB RAM | 128 GB ROM | Expandable Upto 1 TB || 108MP + 2MP | 16MP Front Camera || Qualcomm Snapdragon 695 5G Processor",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 300 + order placed recently",
+        price: 24999,
+        rating: "3.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 5,
+        rname: "Realme 10",
+        imgdata: "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/9/l/3/-original-imaghgcktmwxthzc.jpeg?q=70",
+        address: "8 GB RAM | 128 GB ROM | Expandable Upto 1 TB || 108MP + 2MP | 16MP Front Camera || Qualcomm Snapdragon 695 5G Processor",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "1050 + order placed recently",
+        price: 19990,
+        rating: "4.0",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 6,
+        rname: "Nothing Phone 2",
+        imgdata: "https://rukminim2.flixcart.com/image/312/312/l5h2xe80/mobile/4/7/l/-original-imagg4y3grhaxckf.jpeg?q=70",
+        address: "8 GB RAM | 256 GB ROM  || 4500 mAh Lithium-ion Battery || Qualcomm Snapdragon 8+ Gen 1 Processor ",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 1100 + order placed recently",
+        price: 34999,
+        rating: "3.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 7,
+        rname: "Pixel 7 pro",
+        imgdata: "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/t/u/m/-original-imaggsuehy3nyj3b.jpeg?q=70",
+        address: "8 GB RAM - 128 GB ROM || 50MP (OIS) + 13MP - 13MP Front Camera || 4920 mAh Battery || Tensor G2 Processor",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "500 + order placed  recently",
+        price: 73999,
+        rating: "3.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 8,
+        rname: "Samsung S23",
+        imgdata: "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/1/r/n/-original-imagmg6gyzgzzszt.jpeg?q=70",
+        address: "8 GB RAM | 256 GB ROM || 50MP + 10MP + 12MP | 12MP Front Camera || Qualcomm Snapdragon 8 Gen 2 Processor ",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "500 + order placed recently",
+        price: 79999,
+        rating: "4.2",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 9,
+        rname: "MOTOROLA G62 5G",
+        imgdata: "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/t/6/g/-original-imagh29eknx3cavh.jpeg?q=70",
+        address: "6 GB RAM | 128 GB ROM || 50MP + 8MP + 2MP | 16MP Front Camera || Qualcomm Snapdragon 695 5G Processor   ",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "2525 + order placed recently",
+        price: 15999,
+        rating: "3.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+];
+
+export default Cardsdata;
